4830d6434d3dfedb2a29f5fc2745c2efc93df294
Francisco Matos <junior.mg@gmail.com>
2021-01-17 18:08:14 -0300

    aula 17

---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Comp() {
+    return <Text>Comp #Oficial</Text>
+}
+
+function Comp1() {
+    return <Text>Comp #01</Text>
+}
+
+function Comp2() {
+    return <Text>Comp #02</Text>
+}
+
+export {Comp1, Comp2}
+export default Comp
